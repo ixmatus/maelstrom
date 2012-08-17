673c686afb8d57cbca033447ecfa0e0f7f45283e
Parnell Springmeyer <ixmatus@gmail.com>
2012-08-17 15:48:28 -0500

    Small fix to the start_link

---
+++ src/whirlpool_worker.erl
@@ -0,0 +1,37 @@
+%%%-------------------------------------------------------------------------
+%%% @author    Parnell Springmeyer <ixmatus@gmail.com>
+%%% @copyright 2012 Parnell Springmeyer
+%%% @doc       Poolboy worker instance. Provides a very simple factory worker
+%%%            to handle computations sent with a MFA or FA arg set.
+%%% @end
+%%%-------------------------------------------------------------------------
+-module(whirlpool_worker).
+-behaviour(gen_server).
+-behaviour(poolboy_worker).
+
+-export([start_link/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+start_link(Args) ->
+    gen_server:start_link(?MODULE, Args, []).
+
+init([]) ->
+    {ok, undefined}.
+
+handle_call({compute, {F, A}}, _From, State) ->
+    {reply, erlang:apply(F, A), State};
+
+handle_call({compute, {M, F, A}}, _From, State) ->
+    {reply, erlang:apply(M, F, A), State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
