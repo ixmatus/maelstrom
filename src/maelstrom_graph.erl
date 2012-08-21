cdf45ddcf3ef8d23de639271b41ec5300deb6716
Parnell Springmeyer <ixmatus@gmail.com>
2012-08-21 17:46:40 -0500

    Adding some stuff to the makefile

---
+++ src/maelstrom_graph.erl
@@ -0,0 +1,8 @@
+%%%-------------------------------------------------------------------------
+%%% @author    Parnell Springmeyer <ixmatus@gmail.com>
+%%% @copyright 2012 Parnell Springmeyer
+%%% @doc       Module for managing a maelstrom graph.
+%%% @end
+%%%-------------------------------------------------------------------------
+
+-module(maelstrom_graph).
