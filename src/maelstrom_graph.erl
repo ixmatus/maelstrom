9ce0cad551792ea5bca5211d06dff22868ff5ac9
Parnell Springmeyer <ixmatus@gmail.com>
2012-08-27 00:41:08 -0500

    Removing Erlang files, deciding to go with Haskell

--- src/maelstrom_graph.erl
+++
@@ -1,8 +0,0 @@
-%%%-------------------------------------------------------------------------
-%%% @author    Parnell Springmeyer <ixmatus@gmail.com>
-%%% @copyright 2012 Parnell Springmeyer
-%%% @doc       Module for managing a maelstrom graph.
-%%% @end
-%%%-------------------------------------------------------------------------
-
--module(maelstrom_graph).
